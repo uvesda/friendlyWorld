b42b73b4940bd9da085804ce689405177a8e9e31
uvesda <miiramiirna@gmail.com>
2025-12-14 18:22:08 +0300

    usersModels edited

--- backend/models/usersModels.js
+++ backend/models/usersModels.js
@@ -4,18 +4,23 @@ const SALT_ROUNDS = 10;
 
 module.exports = {
 
-  createUser(name, password) {
-    return new Promise((resolve, reject) => {
-        const salt = bcrypt.genSalt(SALT_ROUNDS);
-        const passwordHash = bcrypt.hash(password, salt);
+  async createUser(name, password) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const salt = await bcrypt.genSalt(SALT_ROUNDS);
+        const passwordHash = await bcrypt.hash(password, salt);
+        
         db.run(`INSERT INTO users (name, passwordHash, salt) VALUES (?, ?, ?)`, 
-            [name, passwordHash, salt],
-            function (err) {
-                if (err) reject(err);
-                else resolve({ id: this.lastID });
-                }
+          [name, passwordHash, salt],
+          function (err) {
+            if (err) reject(err);
+            else resolve({ id: this.lastID, name: name });
+          }
         );
-    })
+      } catch (err) {
+        reject(err);
+      }
+    });
   },
 
   getAllUsers() {
